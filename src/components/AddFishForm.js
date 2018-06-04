af9610b26e13defc8b7e17738c608c6474e1181b
Jason Wise <jasonandrewwise@gmail.com>
2018-06-04 19:39:20 -0400

    trying to get refs to work

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,34 +1,49 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    // 1.  stop the form from submitting
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    };
+    this.props.addFish(fish);
+    // refresh the form
+    event.currentTarget.reset();
+  };
   render() {
     return (
-      <form>
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
         <input
-          name="name
- "
+          name="price"
+          ref={this.priceRef}
           type="text"
-          placeholder=""
+          placeholder="Price"
         />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" />
         <input
-          name="price
-"
+          name="image"
+          ref={this.imageRef}
           type="text"
-          placeholder=""
+          placeholder="Image"
         />
-        <input name="status" type="text" placeholder="" />
-        <input
-          name="desc
- "
-          type="text"
-          placeholder=""
-        />
-        <input
-          name="image
-"
-          type="text"
-          placeholder=""
-        />
+        <button type="submit">+ Add Fish</button>
       </form>
     );
   }
