b24e446c8572324dc463bfe66769fff425a7cba3
Jason Wise <jasonandrewwise@gmail.com>
2018-06-06 21:43:03 -0400

    passing more state

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,15 @@ class App extends React.Component {
       fishes: sampleFishes
     });
   };
+
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Add to order or update to order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call set state to update our state object
+    this.setState({ order: order });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -32,7 +41,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
