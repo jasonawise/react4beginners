f58db2f827d18a4e862b785d9807af404007f8b5
Jason Wise <jasonandrewwise@gmail.com>
2018-06-06 21:04:10 -0400

    passing state

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <li className="menu-fish">
+        <p>Hello Fish</p>
+      </li>
+    );
+  }
+}
+
+export default Fish;
